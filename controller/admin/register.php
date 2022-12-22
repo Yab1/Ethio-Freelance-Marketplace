121721f01cc90dea1520514876bb1704f70261c7
Yab1 <yabilisanu@gmail.com>
2022-12-22 13:31:42 +0300

    reg

---
+++ controller/admin/register.php
@@ -0,0 +1,23 @@
+<?php
+include_once"$_SERVER[DOCUMENT_ROOT]/free/model/admin/register.php";
+
+if(isset($_POST['signup']))
+{
+$fname=$_POST['fname'];
+$lname=$_POST['lname'];
+$dob=date('y-m-d', strtotime($_POST['dob']));
+$username=$_POST['username'];
+$gender=$_POST['gender'];
+$phone=$_POST['phone'];
+$email=$_POST['email'];
+$address=$_POST['address'];
+$password=$_POST['password'];
+$cpassword=$_POST['cpassword'];
+$usertype=$_POST['usertype'];
+
+
+$obj=new Signup($fname,$lname,$dob,$username,$gender,$phone,$email,$address,$password,$cpassword,$usertype);
+$obj->user_Register();
+}
+?>
+
