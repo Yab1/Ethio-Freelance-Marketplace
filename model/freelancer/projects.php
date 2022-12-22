ddb8d551f2f0c24f6f2ab967979fae522c67c404
Yab1 <yabilisanu@gmail.com>
2022-12-22 13:51:16 +0300

    iss

---
+++ model/freelancer/projects.php
@@ -0,0 +1,37 @@
+<?php
+include_once "$_SERVER[DOCUMENT_ROOT]/free/session.php";
+include_once "$_SERVER[DOCUMENT_ROOT]/free/model/db.php";
+
+
+
+class Project_View extends Database{
+    public $project_name;
+    public $category;
+    public $deadline;
+    public $budget;
+    public $employer;
+    public $project_id;
+
+     function view_project(){
+       
+
+$sql = "SELECT * FROM project";
+$result = $this->connect()->query($sql);
+$resultcheck= mysqli_num_rows($result);
+if ($resultcheck > 0) {
+    while($ans=mysqli_fetch_assoc($result)) {
+        $this->project_id=$ans["project_id"];
+        $this->project_name=$ans["project_name"];
+        $this->category=$ans["category"];
+        $this->employer=$ans["e_username"];
+        $this->budget=$ans["budget"];
+        $this->deadline=$ans["posted_on"];       
+    } 
+
+    
+
+}
+
+}
+}
+?>
