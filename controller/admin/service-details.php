b0f7a4b872cacbde7cd06d41fc44a5dbb9506e6c
Yab1 <yabilisanu@gmail.com>
2022-12-22 13:32:29 +0300

    serv

---
+++ controller/admin/service-details.php
@@ -0,0 +1,13 @@
+<?php
+include_once "$_SERVER[DOCUMENT_ROOT]/free/session.php";
+include_once "$_SERVER[DOCUMENT_ROOT]/free/model/admin/service-details.php";
+
+if(isset($_SESSION["s_id"])){
+    $s_id=$_SESSION["s_id"];
+}
+
+$service_detail = new Service_Detail();
+
+$service_detail->ServiceDetail();
+
+?>
